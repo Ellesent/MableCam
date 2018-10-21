84763b698e9583949198941edeb8ef49c7c742eb
Ellesent <francesca.paterinaldi@gmail.com>
2018-10-21 21:51:28 +0000

    fixing issue + commenting out things that require GUI

--- webcam.py
+++ webcam.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy
 
-cv2.namedWindo("preview")
+#cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
 # Try reading first frame
@@ -11,7 +11,7 @@ else:
     rval = False
 
 while rval:
-    cv2.imshow("preview", frame)
+    #cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
