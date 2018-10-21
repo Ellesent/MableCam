b4999e8b6437348ec8d7a1ee750dbc5a544367ed
Ellesent <francesca.paterinaldi@gmail.com>
2018-10-21 22:35:26 +0000

    adding saving webcam output to file

--- webcam.py
+++ webcam.py
@@ -1,20 +1,27 @@
+import numpy as np
 import cv2
-import numpy
-
-#cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
-
-# Try reading first frame
-if vc.isOpened():
-    rval, frame = vc.read()
-else:
-    rval = False
-
-while rval:
-    #cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+ 
+cap = cv2.VideoCapture(0)
+ 
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+ 
+while(cap.isOpened()):
+ 
+    ret, frame = cap.read()
+    if ret==True:
+       # frame = cv2.flip(frame,0)
+        # write the flipped frame
+        out.write(frame)
+       # cv2.imshow('frame',frame)
+ 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
-
-cv2.destroyWindow("preview")
+ 
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
