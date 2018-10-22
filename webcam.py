538d3e602b810956335c3c9d8f44d93b7fc9de87
Ellesent <francesca.paterinaldi@gmail.com>
2018-10-22 00:41:36 +0000

    using flask to send webcam data to client

--- webcam.py
+++ webcam.py
@@ -1,27 +1,33 @@
-import numpy as np
+#!/usr/bin/env python
+from flask import Flask, render_template, Response
 import cv2
- 
-cap = cv2.VideoCapture(0)
- 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
- 
-while(cap.isOpened()):
- 
-    ret, frame = cap.read()
-    if ret==True:
-       # frame = cv2.flip(frame,0)
-        # write the flipped frame
-        out.write(frame)
-       # cv2.imshow('frame',frame)
- 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
- 
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+
+
+app = Flask(__name__)
+vc = cv2.VideoCapture(0)
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def gen():
+    """Video streaming generator function."""
+    while True:
+        rval, frame = vc.read()
+        #ret, jpeg = cv2.imencode('.jpg', frame)
+        cv2.imwrite('t.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False, threaded=True, use_reloader=False)
